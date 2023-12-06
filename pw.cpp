9ed62b9d5569f1dfc8fd1860327d592ecebdeea5
Sagar-108 <sagar.goyal.che20@itbhu.ac.in>
2023-12-06 14:04:03 +0530

    Initial commit - created student class

---
+++ pw.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class students
+{
+    public:
+        int roll_num;
+        char class_name;
+        string food;
+};
+
+vector<int>A_V;
+vector<int>B_V;
+vector<int>A_NV;
+vector<int>B_NV;
+
+unordered_set<int>registered;
+
+
+
+
+int main(){
+
+    int t = 1;
+    cin>>t;
+    while(t--){
+        
+    }
+
+return 0;
+
+}
